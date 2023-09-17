b82327e2ac66017e3a6ecda87ef7090a1b004c06
Panont Anthony <anthonypanont@gmail.com>
2023-09-17 13:59:17 +0200

    Alert (context, reducer, state) + components

--- client/src/App.js
+++ client/src/App.js
@@ -5,29 +5,34 @@ import { Home } from "./components/pages/Home";
 import { About } from "./components/pages/About";
 import { Register } from "./components/auth/Register";
 import { Login } from "./components/auth/Login";
+import { Alerts } from "./components/layout/Alerts";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import ContactState from "./context/contact/ContactState";
 import AuthState from "./context/auth/AuthState";
+import AlertState from "./context/alert/AlertState";
 
 const App = () => {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <Fragment>
-            <Navbar />
-            <div className="container">
-              <Routes>
-                <Route path="/" Component={Home} />
-                <Route path="/about" Component={About} />
-                <Route path="/register" Component={Register} />
-                <Route path="/login" Component={Login} />
-              </Routes>
-            </div>
-          </Fragment>
-        </Router>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className="container">
+                <Alerts />
+                <Routes>
+                  <Route path="/" Component={Home} />
+                  <Route path="/about" Component={About} />
+                  <Route path="/register" Component={Register} />
+                  <Route path="/login" Component={Login} />
+                </Routes>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
