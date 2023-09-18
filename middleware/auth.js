e7a58138ac047d25d108cbd4bc8f1a902d9f0458
Panont Anthony <anthonypanont@gmail.com>
2023-09-18 20:25:30 +0200

    Add notes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,17 +1,19 @@
-const jwt = require("jsonwebtoken");
-const config = require("config");
+const jwt = require("jsonwebtoken"); //La bibilio token
+const config = require("config"); //Pour la secret
 
 module.exports = function (req, res, next) {
-  const token = req.header("x-auth-token");
+  //apres avoir fini on fait next() comme ca il peut aller a un autre middleware
+  const token = req.header("x-auth-token"); //On grab voir s'il y'a un token
 
   if (!token) {
+    //Si pas de token
     return res.status(401).json({ msg: "No token, authorization denied" });
   }
 
   try {
-    const decoded = jwt.verify(token, config.get("jwtSecret"));
+    const decoded = jwt.verify(token, config.get("jwtSecret")); //on verifie le token
 
-    req.user = decoded.user;
+    req.user = decoded.user; //car c'est l'user qui lance la request
     next();
   } catch (err) {
     res.status(401).json({ msg: "Token is not valid" });
