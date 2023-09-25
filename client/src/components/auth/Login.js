a1567ff220db83f12f047d15fa5967378027a160
Panont Anthony <anthonypanont@gmail.com>
2023-09-25 18:30:54 +0200

    Login Alrts / onSubmit / Logout + Navbar

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,27 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+import AlertContext from "../../context/alert/alertContext";
+import {
+  useAuth, //State and dispatcher
+  clearErrors,
+  login,
+} from "../../context/auth/AuthState"; //Destructuration des methodes AuthState
 
 export const Login = () => {
+  const alertContext = useContext(AlertContext);
+  const [authState, authDispatch] = useAuth();
+  const { error, isAuthenticated } = authState;
+
+  const { setAlert } = alertContext;
+
+  useEffect(() => {
+    //A chaque refresh on regarde s'il
+    if (error) {
+      setAlert(error, "danger"); //On envoit l'erreur (txt) avec le style
+      clearErrors(authDispatch); //On clears les erreurs de l'utilisateur, comme ca elle ne s'affiche qu'une fois
+    }
+  }, [error, isAuthenticated, setAlert, authDispatch]);
+
   const [user, setUser] = useState({
     //On cree un state user et un setUser pour la modifier ( pour les valeurs contenues dans le formulaire, ce n'est pas la "véritable state")
     email: "",
@@ -16,9 +37,20 @@ export const Login = () => {
     });
 
   const onSubmit = (e) => {
-    e.preventDefault();
-    console.log("Login submit");
+    e.preventDefault(); //On stop le submit
+    if (email === "" || password === "") {
+      //On check si les input sont remplis (ajouter du regex)
+      setAlert("Please enter all fields", "danger");
+    } else {
+      login(authDispatch, {
+        //On lance la fonction register, avec le dispatch de auth
+        email,
+        password,
+      });
+    }
   };
+
+  if (isAuthenticated) return <Navigate to="/" />; //redirection vers la homepage
 
   return (
     <div className="form-container">
@@ -29,11 +61,18 @@ export const Login = () => {
       <form onSubmit={onSubmit}>
         <div className="form-group">
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" value={email} onChange={onChange} />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            value={email}
+            onChange={onChange}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="password">Password</label>
           <input
+            id="password"
             type="password"
             name="password"
             value={password}
--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -19,6 +19,7 @@ export default (state, action) => {
         user: action.payload,
       };
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem("token", action.payload.token); //On le met en local
       return {
         ...state,
@@ -28,6 +29,8 @@ export default (state, action) => {
       };
     case AUTH_ERROR:
     case REGISTER_FAIL:
+    case LOGIN_FAIL:
+    case LOGOUT:
       localStorage.removeItem("token");
       return {
         ...state,
